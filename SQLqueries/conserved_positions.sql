14d3f6aae3a78fbaf9126f63b64dbd5c351ca0ab
mobi98 <82958725+mobi98@users.noreply.github.com>
2021-10-27 13:09:33 +0100

    Update conserved_positions.sql

--- SQLqueries/conserved_positions.sql
+++ SQLqueries/conserved_positions.sql
@@ -6,29 +6,28 @@ SELECT 	ref_totals.family_group,
         SUM(IF(ref_totals.deleterious < ref_totals.tolerated,1,0)) AS 'tol_positions',
         COUNT(*) total_positions
 FROM
-	(SELECT 	ref_tab.alignment_member_pos,
-			SUBSTRING_INDEX(ref_tab.alignment_member_pos, '_',1) AS 'family_group',
-			SUM(IF(ref_tab.sift_prediction = 'deleterious',1,0)) AS 'deleterious',
-			SUM(IF(ref_tab.sift_prediction = 'tolerated',1,0)) AS 'tolerated',
-			COUNT(*) var_count
+	(SELECT ref_tab.alignment_member_pos,
+		SUBSTRING_INDEX(ref_tab.alignment_member_pos, '_',1) AS 'family_group',
+		SUM(IF(ref_tab.sift_prediction = 'deleterious',1,0)) AS 'deleterious',
+		SUM(IF(ref_tab.sift_prediction = 'tolerated',1,0)) AS 'tolerated',
+		COUNT(*) var_count
 	FROM
-		(SELECT 	ref_positions.alignment_member_pos,
-					variant.variant_id,
-					variant.sift_prediction,
-					variant.polyphen_prediction,
-					variant.m3d_prediction
-			FROM	
-					variant,
-					protein_pos,
-					alignment_member_pos,
-					(SELECT 
-							alignment_member_pos, conservation_score
-						FROM 
-								conservation_score
-						WHERE
-								conservation_score.conservation_score >= 0.7
-						GROUP BY 
-								alignment_member_pos, conservation_score) AS ref_positions
+		(SELECT ref_positions.alignment_member_pos,
+			variant.variant_id,
+			variant.sift_prediction,
+			variant.polyphen_prediction,
+			variant.m3d_prediction
+			FROM	variant,
+				protein_pos,
+				alignment_member_pos,
+				(SELECT 
+					alignment_member_pos, conservation_score
+				 FROM 
+					conservation_score
+				 WHERE
+					conservation_score.conservation_score >= 0.7
+				 GROUP BY 
+					alignment_member_pos, conservation_score) AS ref_positions
 			WHERE
 					variant.protein_pos_id = protein_pos.protein_pos_id
 			AND		protein_pos.protein_pos_id = alignment_member_pos.protein_pos_id
